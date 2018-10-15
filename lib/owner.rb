bf9fcd5f0592bfb6f291f4b8f8c5a542d2e26aa0
Certil Remy <certil@noukod.com>
2018-10-15 18:43:43 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class Owner
   # code goes here
-end
+
+  attr_reader :species
+  attr_accessor :name,:pets
+
+  @@all=[]
+
+  def initialize(name)
+    @name=name
+    @@all << self
+    @species="human"
+    @pets = {:fishes:=>[],:dogs=>[],:cats=>[]}
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all=[]
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(fish_name)
+    fish = Fish.new(fish_name)
+    @pets[:fishes] << fish
+  end
+
+  def buy_dog(dog_name)
+    dog = Dog.new(dog_name)
+    @pets[:dogs] << dog
+    #binding.pry
+  end
